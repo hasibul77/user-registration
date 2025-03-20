cc4686ed2f8180e53087d8082861151900f11f49
hasibul77 <146077292+hasibul77@users.noreply.github.com>
2025-03-20 16:21:09 +0600

    v2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,13 +5,13 @@ const { register, login } = require('../controllers/authController');
 const { validateRegister, validateLogin } = require('../middlewares/validate');
 const { protect } = require('../middlewares/authMiddleware'); 
 
-// Existing registration route
+// Registration route
 router.post('/register', validateRegister, register);
 
 // Login route
 router.post('/login', validateLogin, login);
 
-// PROTECTED ROUTE EXAMPLE
+// PROTECTED Profile route
 router.get('/profile', protect, (req, res) => { // ← USING MIDDLEWARE HERE
   res.json(req.user);
 });
